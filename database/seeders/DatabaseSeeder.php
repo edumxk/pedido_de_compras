20580e5234f1020a1785c89c5a0df423775971c2
edumxk <eduardocsteam@gmail.com>
2024-03-11 14:47:26 -0300

    Versão previa v0.0.7
    ajuste de seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,48 @@ class DatabaseSeeder extends Seeder
     {
 
          \App\Models\User::factory()->create([
-             'name' => 'Eduardo Patrick',
-            'email' => 'teste@teste.com',
-            'nickname' => 'edumxk',
-            'is_admin' => true,
-            'password' => bcrypt('strw4mxk'),
+             [
+                'name' => 'Eduardo Patrick',
+                'email' => 'eduardo.cavalcante@kokar.com.br',
+                'nickname' => 'Eduardo TI',
+                'is_admin' => true,
+                'password' => bcrypt('strw4mxk'),
+            ],
+            [
+                'name' => 'Wanderlei Cichelero',
+                'email' => 'wanderlei@kokar.com.br',
+                'nickname' => 'Wanderlei',
+                'is_admin' => true,
+                'password' => bcrypt('kokar@2024'),
+            ],
+            [
+                'name' => 'Edson Alves',
+                'email' => 'edson@kokar.com.br',
+                'nickname' => 'Edson',
+                'is_admin' => true,
+                'password' => bcrypt('kokar@2024'),
+            ],
+            [
+                'name' => 'Valéria Cardoso',
+                'email' => 'valeria.cardoso@kokar.com.br',
+                'nickname' => 'Valéria',
+                'is_admin' => false,
+                'is_financial' => true,
+                'password' => bcrypt('kokar2024'),
+            ],
+            [
+                'name' => 'Maria Aparecida',
+                'email' => 'mariaaparecida.ribeiro@kokar.com.br',
+                'nickname' => 'Maria Aparecida',
+                'is_admin' => false,
+                'is_financial' => true,
+                'password' => bcrypt('kokar2024')
+                ]
          ]);
 
         $this->call([
             DepartmentsSeeder::class,
-            PositionsSeeder::class,
+            //PositionsSeeder::class,
             ProductCategoriesSeeder::class,
             ProductsSeeder::class,
             ContactsSeeder::class,
