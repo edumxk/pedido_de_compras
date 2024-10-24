704016415c3a7abea9eb8468fa59a2f09a6b0e20
Eduardo Patrick <eduardocsteam@gmail.com>
2024-10-24 12:03:12 -0300

    - fix accordion

--- resources/views/components/budget/card.blade.php
+++ resources/views/components/budget/card.blade.php
@@ -1,5 +1,6 @@
 <div class="dark:bg-gray-800 py-16">
-    <div class="flex items-center justify-between w-full p-5 font-medium text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-product-{{ $budget->id }}" aria-expanded="false" aria-controls="accordion-collapse-body-product-{{ $budget->id }}">
+    <div class="flex items-center hover:cursor-pointer hover:dark:bg-gray-700 hover:bg-gray-200 justify-between w-full p-5 font-medium text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-product-{{ $budget->id }}" aria-expanded="false" aria-controls="accordion-collapse-body-product-{{ $budget->id }}">
+
         <span>
             <label>Range de Preço do Orçamento:</label>
             <label class="text-sm text-gray-900 dark:text-gray-200 text-center">
@@ -13,9 +14,11 @@
             </label>
         </span>
         <span>
-            <label>Fornecedor:</label>
-            <a href="{{ route('budgets.products', $budget->hashedId) }}" class="uppercase text-sm text-gray-900 dark:text-gray-200">
-                {{ $budget->supplier->fantasy_name }}
+            <a href="{{ route('budgets.products', $budget->hashedId) }}" title="Clique para Editar o Orçamento" class="text-sm text-gray-900 dark:text-gray-200 p-6 hover:dark:bg-gray-500">
+                <label>Fornecedor:</label>
+                <label class="text-sm uppercase text-gray-900 dark:text-gray-200">
+                    {{ $budget->supplier->fantasy_name }}
+                </label>
             </a>
             @if($budget->status == 'approved')
                 <span class="bg-green-500 text-white font-bold py-2 px-4 rounded">Aprovado</span>
@@ -36,7 +39,7 @@
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
         </svg>
     </div>
-    <div id="accordion-collapse-body-product-{{ $budget->id }}" class="hidden">
+    <div id="accordion-collapse-body-product-{{ $budget->id }}" class="">
         <x-budget.products :budget="$budget"/>
         <x-budget.budget-approve :budget="$budget"/>
     </div>
